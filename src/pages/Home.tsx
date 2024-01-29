eb77541acb576d3a29b0c574da46ee4d6febc4dd
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2024-01-29 14:29:16 +0700

    chore: typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     <>
       <div className=" flex flex-col justify-center  items-center h-[calc(100vh-64px)]">
         <div className="text-2xl font-bold">Impact Calculator</div>
-        <Link to="/Category" className="mt-2">
+        <Link to="/category" className="mt-2">
           Enter site
         </Link>
       </div>
