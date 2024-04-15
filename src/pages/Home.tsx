54505fd073bbfc6299175cf1ff0d3378c2840daf
Billy19191 <bill.ratchanon@gmail.com>
2024-04-15 23:55:57 +0700

    chore: change name to calculator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: FC = () => {
         <div className="hero-content flex-col lg:flex-row-reverse mx-40">
           <img src="img/sunny.svg" alt="pheonix" className="max-w-sm w-64" />
           <div>
-            <h1 className="text-5xl font-bold">Impact Catalyzt</h1>
+            <h1 className="text-5xl font-bold">Impact Caculator</h1>
             <p className="font-normal py-6 lg:pr-12 w-[calc(100%-16rem)]">
               The Impact Calculator is designed to streamline the evaluation of
               projects in the Optimism RetroPGF voting process by quantifying
